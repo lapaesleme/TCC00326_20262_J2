4cbc1466aeacd5e0b01a50118b39fdcfc1a0125b
Luiz André Portes Paes Leme <lapaesleme@ic.uff.br>
2026-08-25 17:45:16 -0300

    ok

--- src/trabalhos/felipexavier/Lista 1/lista 1.py
+++ src/trabalhos/felipexavier/Lista 1/lista 1.py
@@ -1,64 +1,64 @@
 # %%
-#Lista 1, 3.4
+#lista1, 3.4
 #Soma ate zero
 
 
 
 # %%
-#Lista 1, 1.1
+#lista1, 1.1
 #Maioridade
 
 
 
 # %%
-#Lista 1, 1.2
+#lista1, 1.2
 #Numero positivo, negativo ou zero
 
 
 
 # %%
-#Lista 1, 1.3
+#lista1, 1.3
 #Par ou impar
 
 
 
 # %%
-#Lista 1, 1.4
+#lista1, 1.4
 #M�dia escolar
 
 
 
 # %%
-#Lista 1, 1.5
+#lista1, 1.5
 #Calculadora simples
 
 
 
 # %%
-#Lista 2, 2.1
+#lista2, 2.1
 #Contagem de 1 a 10
 
 
 # %%
-#Lista 2, 2.2
+#lista2, 2.2
 #Tabuada
 
 
 
 # %%
-#Lista 2, 2.3
+#lista2, 2.3
 #soma de n�meros
 
 
 
 # %%
-#Lista 2, 2.4
+#lista2, 2.4
 #contagem regressiva
 
 
 
 # %%
-#Lista 2, 2.5
+#lista2, 2.5
 #Vogais em uma palavra
 
 
@@ -76,6 +76,6 @@
 
 
 # %%
-#Lista 1, 3.3
+#lista1, 3.3
 #Advinhe o numero
 
