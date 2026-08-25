60cfb2aa980d3c8dd896603375b8ace433f002fb
Berimbaud <berimbaud@gmail.com>
2026-08-25 17:26:14 -0300

    Add performance evaluation based on average score

---
+++ src/trabalhos/felipexavier/lista2/ex1.py
@@ -0,0 +1,15 @@
+numeros = [8,5,6,5,5,4,8,9,7,8]
+media = 0
+for i in range(0,len(numeros)):
+
+    media = media + numeros[i] / len(numeros)
+
+
+if media >= 7:
+    print(f"Desempenho: Bom e média: {media}")
+elif media < 7 and media >= 5:
+    print(f"Desempenho: Médio e média: {media}")
+elif media < 5:
+    print(f"Desempenho: Baixo e média: {media}")
+
+
