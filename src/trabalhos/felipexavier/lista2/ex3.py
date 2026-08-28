018038c0b83f9e0fbccfa7205fc4ec301fefe96d
Berimbaud <berimbaud@gmail.com>
2026-08-28 03:29:06 -0300

    Update ex3.py

---
+++ src/trabalhos/felipexavier/lista2/ex3.py
@@ -0,0 +1,22 @@
+# %%
+#Esse é osso
+def eh_primo(n):
+
+    if n < 2:
+        return False
+
+    for i in range(2, n):
+
+        if n % i == 0:
+            return False
+
+    return True
+
+
+inicio = int(input("Digite o primeiro número: "))
+fim = int(input("Digite o segundo número: "))
+
+for numero in range(inicio, fim + 1):
+
+    if eh_primo(numero):
+        print(numero)
