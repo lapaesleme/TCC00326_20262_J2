3328b7c1f8d58b9b6ab8aee13f94ab54b78b18f8
Luiz André Portes Paes Leme <lapaesleme@ic.uff.br>
2026-09-25 14:39:52 -0300

    ok

--- src/trabalhos/luizandre/testes/teste1.py
+++ src/trabalhos/luizandre/testes/teste1.py
@@ -1,18 +1,11 @@
+m1 = [[1, 2], [3, 4], [5, 6]]
+m2 = [[2, 3], [4, 5], [6, 7]]
+resultado = []
 
+for i in range(len(m1)):
+    linha = []
+    for j in range(len(m1[0])):
+        linha.append(m1[i][j] + m2[i][j])
+    resultado.append(linha)
 
-var = {"nome": "Luiz"}
-#var = dict()
-
-var["tel"]=99999999
-var["nome"]="Andre"
-
-print(list(var.items()))
-
-for par in list(var.items()):
-    print(par)
-
-for chave, valor in list(var.items()):
-    print(chave, valor)
-
-
-
+print(resultado)
