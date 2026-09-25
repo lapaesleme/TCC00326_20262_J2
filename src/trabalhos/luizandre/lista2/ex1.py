225fc2c51e1be5d29ee4743d444efc78d9ccc79c
Luiz André Portes Paes Leme <lapaesleme@ic.uff.br>
2026-09-25 14:55:49 -0300

    ok

--- src/trabalhos/luizandre/lista2/ex1.py
+++ src/trabalhos/luizandre/lista2/ex1.py
@@ -1,43 +1,28 @@
-numeros = [1, 43, -4, 6, 2, 10, 8, 78, 100, 56]
+numeros = [1, 43, -4, 6, 2, 10, 8, 0, 78, 100, 56]
 
-positivos = 0
-negativos = 0
-zeros = 0
-pares = 0
-impares = 0
 
-for numero in numeros:
-    if numero > 0:
-        positivos = positivos + 1
-    elif numero < 0:
-        negativos = negativos + 1
-    else:
-        zeros = zeros + 1
+def estatisticas(numeros):
+    positivos = 0
+    negativos = 0
+    zeros = 0
+    pares = 0
+    impares = 0
 
-    if numero % 2 == 0:
-        pares = pares + 1
-    else:
-        impares = impares + 1
+    for numero in numeros:
+        if numero > 0:
+            positivos = positivos + 1
+        elif numero < 0:
+            negativos = negativos + 1
+        else:
+            zeros = zeros + 1
 
-positivos = 0
-negativos = 0
-zeros = 0
-pares = 0
-impares = 0
+        if numero % 2 == 0:
+            pares = pares + 1
+        else:
+            impares = impares + 1
 
-for i in range(0, len(numeros)):
-    if numeros[i] > 0:
-        positivos = positivos + 1
-    elif numeros[i] < 0:
-        negativos = negativos + 1
-    else:
-        zeros = zeros + 1
-    if numeros[i] % 2 == 0:
-        pares = pares + 1
-    else:
-        impares = impares + 1
+    estatisticas = {"pares": pares, "impares": impares, "zeros": zeros, "positivos": positivos, "negativos": negativos}
+    return estatisticas
 
-dict = {"pares": pares, "impares": impares, "zeros": zeros, "positivos": positivos, "negativos": negativos}
 
-print(dict["pares"])
-print(dict)
+print(estatisticas(numeros))
