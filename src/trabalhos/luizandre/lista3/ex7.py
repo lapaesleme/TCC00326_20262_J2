09f4e03478ebc87b03742f52497ad5fe117dd2b9
Luiz André Portes Paes Leme <lapaesleme@ic.uff.br>
2026-09-25 15:07:46 -0300

    ok

--- src/trabalhos/luizandre/lista3/ex7.py
+++ src/trabalhos/luizandre/lista3/ex7.py
@@ -1,6 +1,8 @@
 ativos = [
-    {"nome": "ibov", "qtd": 10, "compra": 123.0, "preco": 134},
-    {"nome": "itau", "qtd": 12, "compra": 12.0, "preco": 48}
+    {"nome": "aaaa", "qtd": 10, "compra": 123.0, "preco": 134},
+    {"nome": "bbbb", "qtd": 12, "compra": 12.0, "preco": 12.6},
+    {"nome": "cccc", "qtd": 15, "compra": 20.0, "preco": 25},
+    {"nome": "dddd", "qtd": 17, "compra": 30, "preco": 12.6}
 ]
 
 
@@ -20,5 +22,23 @@ def rentabilidade(ativo):
     return (valor_atual(ativo) / valor_investido(ativo) - 1) * 100
 
 
+def melhor_pior(ativos):
+    maior = 0
+    menor = 9999
+    nomeMaior = None
+    nomeMenor = None
+    for ativo in ativos:
+        percentual = rentabilidade(ativo)
+        if percentual > maior:
+            meior = percentual
+            nomeMaior = ativo["nome"]
+        if percentual < menor:
+            menor = percentual
+            nomeMenor = ativo["nome"]
+    return {"melhor": nomeMaior, "pior": nomeMenor}
+
+
 for ativo in ativos:
     print(ativo["nome"], "resultado", rentabilidade(ativo))
+
+print(melhor_pior(ativos))
